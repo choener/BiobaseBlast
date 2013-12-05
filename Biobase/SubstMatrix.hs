010713fded0260efad821d171c6ef23e54d5804b
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-12-05 03:11:12 +0100

    minimalistic library to read blast-stuff
    currently only blosum and pam matrices

--- Biobase/SubstMatrix.hs
+++ Biobase/SubstMatrix.hs
@@ -1,2 +1,8 @@
 
 module Biobase.SubstMatrix where
+
+import qualified Data.Array.IArray as A
+
+
+
+type SubstMatrix = A.Array (Char,Char) Int
--- Biobase/SubstMatrix/Import.hs
+++ Biobase/SubstMatrix/Import.hs
@@ -1,5 +1,17 @@
 
 module Biobase.SubstMatrix.Import where
 
+import qualified Data.Array.IArray as A
+import Control.Applicative
+
 import Biobase.SubstMatrix
+
+
+
+fromFile :: FilePath -> IO SubstMatrix
+fromFile fname = do
+  (x:xs) <- dropWhile (("#"==).take 1) . lines <$> readFile fname
+  let cs = map head . words $ x -- should give us the characters encoding an amino acid
+  let ss = map (map read . drop 1 . words) $ xs
+  return $ A.accumArray (\_ z -> z) (-10) (('*','*'),('Z','Z')) [ ((k1,k2),z) | (k1,s) <- zip cs ss, (k2,z) <- zip cs s ]
 
