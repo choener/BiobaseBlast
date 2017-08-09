7708dd7aefa4d595a0565e2ce01d3eb0e705bf1f
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-08-09 18:14:14 +0200

    Improved tests to show diff on failure

--- tests/properties.hs
+++ tests/properties.hs
@@ -5,10 +5,12 @@
 module Main where
 
 import Test.Tasty
-import Test.Tasty.Silver
+import Test.Tasty.Silver.Advanced
 import Biobase.BLAST.Import
 import System.FilePath ( replaceExtension )
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Control.Monad
 
 main :: IO ()
 main = defaultMain =<< allTests
@@ -27,9 +29,16 @@ succeedTest = do
 tastyTest :: FilePath -> TestTree
 tastyTest testFile =
   let
-    goldenFile :: FilePath
-    goldenFile = replaceExtension testFile ".golden"
-    in goldenVsAction "Tabularblast" goldenFile (blastFromFile testFile) (\a -> T.pack $ concatMap show a)
+    goldenFilePath :: FilePath
+    goldenFilePath = replaceExtension testFile ".golden"
+    in goldenTest1 "Tabular HTTP blast" (liftM Just (TIO.readFile goldenFilePath)) (testBlast testFile) compareText (ShowText) print
+    --in goldenVsAction "Tabularblast" goldenFile (blastFromFile testFile) (\a -> T.pack $ concatMap show a)
+
+testBlast :: FilePath -> IO T.Text
+testBlast testFile = do
+  result <- blastFromFile testFile
+  let textresult = T.pack $ ((concatMap show result)++ "\n")
+  return textresult
 
 succeedHTTPTest :: IO TestTree
 succeedHTTPTest = do
@@ -38,6 +47,18 @@ succeedHTTPTest = do
 tastyHTTPTest :: FilePath -> TestTree
 tastyHTTPTest testFile =
   let
-    goldenFile :: FilePath
-    goldenFile = replaceExtension testFile ".golden"
-    in goldenVsAction "Tabular HTTP blast" goldenFile (blastHTTPFromFile testFile) (\a -> T.pack $ concatMap show a)
+    goldenFilePath :: FilePath
+    goldenFilePath = replaceExtension testFile ".golden"
+    in goldenTest1 "Tabular HTTP blast" (liftM Just (TIO.readFile goldenFilePath)) (testBlastHTTPS testFile) compareText (ShowText) print
+  --in goldenVsAction "Tabular HTTP blast" goldenFile (blastHTTPFromFile testFile) (\a -> T.pack $ concatMap show a)
+
+testBlastHTTPS :: FilePath -> IO T.Text
+testBlastHTTPS testFile = do
+  result <- blastHTTPFromFile testFile
+  let textresult = T.pack $ ((concatMap show result) ++ "\n")
+  return textresult
+
+compareText :: T.Text -> T.Text -> GDiff
+compareText goldT testT
+  | goldT == testT = Equal
+  | otherwise = DiffText (Just ("Golden file and test output are different:\nTest:\n" ++ T.unpack testT ++ "\nGold:\n" ++ T.unpack goldT)) testT goldT
