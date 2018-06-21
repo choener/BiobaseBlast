eecf80e848bcfa0892d773ce811ae21fcc5b596c
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-22 00:17:55 +0200

    improved default.nix

--- default.nix
+++ default.nix
@@ -2,26 +2,22 @@ with (import <nixpkgs> {});
 with haskell.lib;
 
 rec {
-  hsPkgs = haskellPackages.extend (packageSourceOverrides {
-    BiobaseBlast = ./.;
-    BiobaseENA = ../Lib-BiobaseENA;
-    BiobaseTypes = ../Lib-BiobaseTypes;
-    BiobaseXNA = ../Lib-BiobaseXNA;
-    DPutils = ../Lib-DPutils;
-    ForestStructures = ../Lib-ForestStructures;
-    OrderedBits = ../Lib-OrderedBits;
-    PrimitiveArray = ../Lib-PrimitiveArray;
-    SciBaseTypes = ../Lib-SciBaseTypes;
-  });
+  hsSrcSet = (lib.foldl' (s: p: s // (import p).hsSrcSet) {} [
+    ../Lib-BiobaseENA
+    ../Lib-BiobaseTypes
+    ../Lib-BiobaseXNA
+    ../Lib-PrimitiveArray
+    ../Lib-SciBaseTypes
+  ]) // {BiobaseBlast = ./.;};
+  hsPkgs = haskellPackages.extend (packageSourceOverrides hsSrcSet);
   hsShell = with hsPkgs; shellFor {
     packages = p: [ p.BiobaseBlast ];
     withHoogle = true;
     buildInputs = [
       cabal-install ghc
-      BiobaseENA BiobaseTypes BiobaseXNA
-      DPutils
-      ForestStructures
-      OrderedBits
+      BiobaseENA
+      BiobaseTypes
+      BiobaseXNA
       PrimitiveArray
       SciBaseTypes
     ];
