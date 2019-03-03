a3f24ac18426c0f91d901b64c42556a519a8d48d
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:39 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,25 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, aeson, attoparsec, base, binary, BiobaseENA
+, BiobaseTypes, BiobaseXNA, bytestring, cereal, containers, deepseq
+, directory, file-embed, filepath, lens, log-domain, mtl
+, PrimitiveArray, SciBaseTypes, split, stdenv, tasty
+, tasty-quickcheck, tasty-silver, tasty-th, text
+, unordered-containers, vector, vector-th-unbox
+}:
+mkDerivation {
+  pname = "BiobaseBlast";
+  version = "0.3.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson attoparsec base binary BiobaseENA BiobaseTypes BiobaseXNA
+    bytestring cereal containers deepseq directory file-embed lens
+    log-domain mtl PrimitiveArray SciBaseTypes text
+    unordered-containers vector vector-th-unbox
+  ];
+  testHaskellDepends = [
+    base bytestring containers filepath split tasty tasty-quickcheck
+    tasty-silver tasty-th text
+  ];
+  homepage = "https://github.com/choener/BiobaseBlast";
+  description = "BLAST-related tools";
+  license = stdenv.lib.licenses.gpl3;
+}
